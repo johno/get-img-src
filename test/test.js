e4b5c3496f75ca590eb153055d996038358f9bb2
John Otander <johnotander@gmail.com>
2015-01-18 22:58:24 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var getImgSrc = require('..');
+
+describe('get-img-src', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(camelizedPackageName(), true);
+  });
+});

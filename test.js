c465339048d49d57c689f4fd7e2a4d8ffab3d775
John Otander <johnotander@gmail.com>
2015-10-19 11:32:02 -0600

    Use ava

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
-var assert = require('assert');
-var getImgSrc = require('./');
+import test from 'ava'
+import getImgSrc from './'
 
-describe('get-img-src', function() {
+test('should return img src attributes wtesthin double quotes when they exist', t => {
+  t.same(getImgSrc('<img class="foo" src="some-image.jpg"></img>'), ['some-image.jpg'])
+})
 
-  it('should return img src attributes within double quotes when they exist', function () {
-    assert.deepEqual(getImgSrc('<img class="foo" src="some-image.jpg"></img>'), ['some-image.jpg'])
-  });
+test('should return multiple img src attributes wtesthin single quotes when they exist', t => {
+  t.same(
+    getImgSrc('<img class="foo" src=\'some-image.jpg\'></img>' +
+              '<img class="foo" src="some-other-image.jpg"></img>'),
+    ['some-image.jpg', 'some-other-image.jpg']
+  )
+})
 
-  it('should return multiple img src attributes within single quotes when they exist', function () {
-    assert.deepEqual(
-      getImgSrc('<img class="foo" src=\'some-image.jpg\'></img>' +
-                '<img class="foo" src="some-other-image.jpg"></img>'),
-      ['some-image.jpg', 'some-other-image.jpg']
-    )
-  });
-
-  it('should return an empty array when there are no images', function () {
-    assert.deepEqual(getImgSrc('<span>HTML without an image</span> <img class="foo"></img>'), [])
-  });
-});
+test('should return an empty array when there are no images', t => {
+  t.same(getImgSrc('<span>HTML wtesthout an image</span> <img class="foo"></img>'), [])
+})

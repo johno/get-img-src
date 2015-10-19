14a59a95f7c86837e17b3b2ba14575dcc8df87c2
John Otander <johnotander@gmail.com>
2015-10-19 11:41:30 -0600

    Tweak test wording

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import test from 'ava'
 import getImgSrc from './'
 
-test('should return img src attributes wtesthin double quotes when they exist', t => {
+test('returns img src attributes wtesthin double quotes when they exist', t => {
   t.same(getImgSrc('<img class="foo" src="some-image.jpg"></img>'), ['some-image.jpg'])
 })
 
-test('should return multiple img src attributes wtesthin single quotes when they exist', t => {
+test('returns multiple img src attributes wtesthin single quotes when they exist', t => {
   t.same(
     getImgSrc('<img class="foo" src=\'some-image.jpg\'></img>' +
               '<img class="foo" src="some-other-image.jpg"></img>'),
@@ -13,6 +13,6 @@ test('should return multiple img src attributes wtesthin single quotes when they
   )
 })
 
-test('should return an empty array when there are no images', t => {
+test('returns an empty array when there are no images', t => {
   t.same(getImgSrc('<span>HTML wtesthout an image</span> <img class="foo"></img>'), [])
 })

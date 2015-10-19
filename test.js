151c5dfdd5731ed9eac583d4eb9cfc02d179518f
John Otander <johnotander@gmail.com>
2015-10-19 11:42:32 -0600

    Add t.plan

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ import test from 'ava'
 import getImgSrc from './'
 
 test('returns img src attributes wtesthin double quotes when they exist', t => {
+  t.plan(1)
+
   t.same(getImgSrc('<img class="foo" src="some-image.jpg"></img>'), ['some-image.jpg'])
 })
 
 test('returns multiple img src attributes wtesthin single quotes when they exist', t => {
+  t.plan(1)
+
   t.same(
     getImgSrc('<img class="foo" src=\'some-image.jpg\'></img>' +
               '<img class="foo" src="some-other-image.jpg"></img>'),
@@ -14,5 +18,7 @@ test('returns multiple img src attributes wtesthin single quotes when they exist
 })
 
 test('returns an empty array when there are no images', t => {
+  t.plan(1)
+
   t.same(getImgSrc('<span>HTML wtesthout an image</span> <img class="foo"></img>'), [])
 })
